73e24903c595b116a56d846751dab7f82708124f
peter-fehervari <fpeter17@gmail.com>
2020-10-13 12:38:17 +0200

    1st

---
+++ midterm_master_code.R
@@ -0,0 +1,16 @@
+########### Libraries #############
+library(xlsx)
+
+########### Description #############
+
+# The dataset is dogs with babesiosis tests. All values are randomized for each iteration.
+# Data: 
+  # ID: 1:n values
+  # name: randomized from list (Dog_names.csv)
+  # sex: randomized 
+  # age: random value in months, range 10-156
+  # weight: random from normal dist, with different mu and sigma for the sexes
+  # protected against ticks?
+  # tick confirmed
+  # body temp: in celsius 38-39.2, fever upto 41  
+  # status: blood slides
